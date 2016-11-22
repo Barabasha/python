6e527851ec4c0beb931e50d6d3c0fa9677a04cc1
Dmitriy <dbhost@mail.ru>
2016-11-22 15:33:37 +0200

    Add files via upload

---
+++ class_stats.py
@@ -0,0 +1,101 @@
+
+group = {
+    1024: {"fullname": "Грицаенко Евгений",  "email": "", "github": "https://github.com/ZhenyaGricaenko", "rank": 0},
+    1025: {"fullname": "Гамов Олег",         "email": "", "github": "https://github.com/Aminazine", "rank": 0},
+    1026: {"fullname": "Андреева Ольга",     "email": "", "github": "", "rank": 0},
+    1027: {"fullname": "Алексеенко Наталия", "email": "", "github": "", "rank": 0},
+    1028: {"fullname": "Петина Ольга",       "email": "", "github": "", "rank": 0},
+    1029: {"fullname": "Балан Николай",      "email": "", "github": "https://github.com/Slipy901",  "rank": 0},
+    1030: {"fullname": "Кривенко Максим",    "email": "", "github": "https://github.com/Maksym444", "rank": 0},
+    1031: {"fullname": "Локтев Владимир",    "email": "", "github": "https://github.com/4oc3p",     "rank": 0},
+    1032: {"fullname": "Сорокина Татьяна",   "email": "", "github": "", "rank": 0},
+    1033: {"fullname": "Юрин Руслан",        "email": "", "github": "", "rank": 0},
+    1034: {"fullname": "Румянцев Аркадий",   "email": "", "github": "", "rank": 0},
+    1035: {"fullname": "Торбеев Алексей",    "email": "", "github": "", "rank": 0},
+    1036: {"fullname": "Сташук Екатерина",   "email": "", "github": "", "rank": 0}
+}
+
+hw_results = [
+    {"id": 1024, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] },
+    {"id": 1025, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1,0,0,0,0,0] },
+    {"id": 1026, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1027, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0] },
+    {"id": 1028, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1029, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1030, "task_completion": [1,1,1,1,1,1,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,0,0,0,0] },
+    {"id": 1031, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1] },
+    {"id": 1032, "task_completion": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1033, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1034, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1035, "task_completion": [1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] },
+    {"id": 1036, "task_completion": [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] }
+]
+
+
+test1_results = [
+    {"id": 1024, "task_completion": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+    {"id": 1025, "task_completion": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+    {"id": 1026, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1027, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1028, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1029, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1030, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1031, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1032, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1033, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1034, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1035, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]},
+    {"id": 1036, "task_completion": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+]
+
+test1_costs = {
+    1 : 1,
+    2 : 1,
+    3 : 1,
+    4 : 2,
+    5 : 2,
+    6 : 2,
+    7 : 4,
+    8 : 4,
+    9 : 4,
+    10 : 8,
+    11 : 8,
+    12 : 15
+}
+
+print(group)
+
+def update_students_results():
+    '''
+    Calculate student results and put it into the student dictionary under the key "rank"
+    Total rank is calculated as a sum of completed hw tasks +
+        sum of completed Test1 tasks weighted proportional to its costs
+        For example, student with id=1024 has total rank = 1*29 + (1*1 + 1*1 + 1*1 + ... 1*15 = 81))
+    :return: None
+    '''
+    pass
+
+def print_students_info(sort_by_key="fullname"):
+    '''
+    Prints students info sorted according to the passed key in dictionary). By default, sort by students names
+    Student info should be presented as a card that includes the following information:
+        - id,
+        - name,
+        - email,
+        - github account (only name, not URL path)
+        - rank
+    Example:
+        ----------------------------------------
+        : ID:                               1025
+        :
+        : Full name:                  Гамов Олег
+        : Email:             o.v.gamov@gmail.com
+        : Github:                      Aminazine
+        : Rank:                               23
+        ----------------------------------------
+
+    :return: None
+    '''
+    pass
+
+
