83cb6fd4d170c4f05310615af0958daf9e19016e
Dmitriy <dbhost@mail.ru>
2017-05-10 15:08:50 +0300

    Update phone_book.py

--- phone_book.py
+++ phone_book.py
@@ -1,24 +1,29 @@
 import pickle
 
-phone_book = [{"surname": "Poroshenko", "name": "Petr", "age": 50}]
+phone_book = [
+              {"name": "Petr", "surname": "Petrov", "age": 50, "phone_number":"+380501234567"},
+              {"name": "Ivan", "surname": "Ivanov", "age": 15, "phone_number":"+380507654321"},
+             ]
 
 def print_entry(number, entry):
-    print "[ " + str(number) + " ]" + "-----------------"
-    print "Surname:    " + entry["surname"]
-    print "Name:    " + entry["name"]
-    print "Age:     " + str(entry["age"])
+    print ("[ " + str(number) + " ]" + "-----------------")
+    print ("Surname:    " + entry["surname"])
+    print ("Name:    " + entry["name"])
+    print ("Age:     " + str(entry["age"]))
 
 
 def print_phonebook():
-    print ""
-    print "###### Phone book #######"
-    print ""
+    print ()
+    print ("###### Phone book #######")
+    print ()
 
     number = 1
     for entry in phone_book:
         print_entry(number, entry)
         number += 1
 
+def print_phonebook_by_age():
+    pass
 
 def add_entry_phonebook(surname, name, age):
     entry = {}
@@ -27,107 +32,48 @@ def add_entry_phonebook(surname, name, age):
     entry["age"] = age
     phone_book.append(entry)
 
+def printError(message):
+    print ("ERROR: %s" % message)
 
-def printMessage(message):
-    print "MESSAGE: %s" % message
-
+def printInfo(message):
+    print ("INFO: %s" % message)
 
 def find_entry_name_phonebook(name):
+    idx = 1
+    found = False
     for entry in phone_book:
         if entry["name"] == name:
-            print_entry(1, entry)
-            return
-    printMessage("Not found!!")
+            print_entry(idx, entry)
+            idx += 1
+            found = True
+    if not found:
+        printError("Not found!!")
 
 
 def find_entry_age_phonebook(age):
-    for entry in phone_book:
-        if entry["age"] == age:
-            print_entry(1, entry)
-            return
-    printMessage("Not found!!")
-
+    pass
 
 def delete_entry_name_phonebook(name):
-    for entry in phone_book:
-        if entry["name"] == name:
-            phone_book.remove(entry)
-            return
-    printMessage("Not found!!")
-
+    pass
 
 def count_all_entries_in_phonebook():
-    print len(phone_book)
+    print ("Total number of entries: ", len(phone_book))
 
+def print_avr_age():
+    pass
 
-def print_all_pensioners():
-    number = 1
-    for entry in phone_book:
-        if isPensioner(entry):
-            print_entry(number, entry)
-            number += 1
-
-def count_all_pensioners():
-    pensioners = 0
-    for entry in phone_book:
-        if isPensioner(entry):
-            pensioners += 1
-    print pensioners
-
-
-def avr_age_of_all_pensioners():
-    pensioners = []
-    for entry in phone_book:
-        if isPensioner(entry):
-            pensioners.append(entry)
-    total_age = 0
-    for entry in pensioners:
-        total_age += entry.get("age")
-    print total_age / len(pensioners)
-
-def print_all_students():
-    number = 1
-    for entry in phone_book:
-        if isStudent(entry):
-            print_entry(number, entry)
-            number += 1
-def increase_age(nmbr_of_years):
-    for entry in phone_book:
-        entry["age"] = entry.get("age") + nmbr_of_years
-
-def update_name():
-    for entry in phone_book:
-        if isStudent(entry):
-            entry["name"] = entry["name"] + ' - student'
-
-    for entry in phone_book:
-        if isPensioner(entry):
-            entry["name"] = entry["name"] + ' - pensioner'
-
-def func(item):
-    return item["age"]
-
-def sorted_by_age():
-    phone_book = sorted(phone_book, key=func)
-    print phone_book
-
-def isStudent(entry):
-    return entry["age"] in range(17, 26)
-
-
-def isPensioner(entry):
-    return entry["age"] >= 65
-
+def increase_age(number_of_years):
+    pass
 
 def save_to_file():
     pickle.dump(phone_book, open("phone_book.save", "wb"))
-    printMessage("Phonebook is saved into 'phone_book.save'")
+    printInfo("Phonebook is saved into 'phone_book.save'")
 
 
 def load_from_file():
     global phone_book
     phone_book = pickle.load(open("phone_book.save", "rb"))
-    printMessage("Phonebook is loaded from 'phone_book.save'")
+    printInfo("Phonebook is loaded from 'phone_book.save'")
 
 
 def main():
@@ -137,21 +83,17 @@ def main():
             print ""
             print ""
             print ""
-            print "~ Welcome to Dmitry Bogach phonebook ~"
+            print "~ Welcome to phonebook ~"
             print "Select one of actions below:"
             print "     1 - Print phonebook entries"
-            print "     2 - Add new entry"
-            print "     3 - Find entry by name"
-            print "     4 - Find entry by age"
-            print "     5 - Delete entry by name"
-            print "     6 - The number of entries in the phonebook"
-            print "     7 - Print all pensioners"
-            print "     8 - The number of all pensioners"
-            print "     9 - Avr. age of all pensioners"
-            print "     10 - Print all students"
-            print "     11 - Increase age"
-            print "     12 - Update name"
-            print "     13 - Sorted by age"
+            print "     2 - Print phonebook entries (by age)"
+            print "     3 - Add new entry"
+            print "     4 - Find entry by name"
+            print "     5 - Find entry by age"
+            print "     6 - Delete entry by name"
+            print "     7 - The number of entries in the phonebook"
+            print "     8 - Avr. age of all persons"
+            print "     9 - Increase age"
             print "-----------------------------"
             print "     s - Save to file"
             print "     l - Load from file"
@@ -163,41 +105,33 @@ def main():
             if choice == 1:
                 print_phonebook()
             elif choice == 2:
+                print_phonebook_by_age()
+            elif choice == 3:
                 surname = str(raw_input("    Enter surname: "))
                 name = str(raw_input("    Enter name: "))
                 age = int(raw_input("    Enter age: "))
                 add_entry_phonebook(surname, name, age)
-            elif choice == 3:
+            elif choice == 4:
                 name = str(raw_input("    Enter name: "))
                 find_entry_name_phonebook(name)
-            elif choice == 4:
+            elif choice == 5:
                 age = int(raw_input("    Enter age: "))
                 find_entry_age_phonebook(age)
-            elif choice == 5:
+            elif choice == 6:
                 name = str(raw_input("    Enter name: "))
                 delete_entry_name_phonebook(name)
-            elif choice == 6:
-                count_all_entries_in_phonebook()
             elif choice == 7:
-                print_all_pensioners()
+                count_all_entries_in_phonebook()
             elif choice == 8:
-                count_all_pensioners()
+                avr_age_of_all_persons()
             elif choice == 9:
-                avr_age_of_all_pensioners()
-            elif choice == 10:
-                print_all_students()
-            elif choice == 11:
                 nmbrs_of_years = int(raw_input("    Enter number of years: "))
                 increase_age(nmbrs_of_years)
-            elif choice == 12:
-                update_name()
-            elif choice == 13:
-                sorted_by_age()
             elif choice == 0:
                 print "Bye!"
                 break
             else:
-                print "Enter action within range [0..8]"
+                print "Enter action within range [0..9]"
 
         except ValueError:
             if str(user_input) == 's':
@@ -205,7 +139,7 @@ def main():
             elif str(user_input) == 'l':
                 load_from_file()
             else:
-                print "Oops!  Something went wrong.  Try again..."
+                printError("Something went wrong. Try again...")
 
 
 if __name__ == '__main__':
